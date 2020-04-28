d92c6aad2f803120c053bb8a51d04ea50c106e80
NiteshBabu <NiteshBabu@yahoo.com>
2020-04-28 18:00:06 +0530

    default pic changed

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 class Profile (models.Model):
 	user = models.OneToOneField(User, on_delete = models.CASCADE)
-	image = models.ImageField(default = 'default.jpg', upload_to = 'profile_pics')
+	image = models.ImageField(default = 'default.png', upload_to = 'profile_pics')
 
 	# overriding the save method of base class
 	def save(self, *args, **kwargs):
@@ -16,8 +16,8 @@ class Profile (models.Model):
 		
 		img = Image.open(self.image.path)
 
-		if img.height > 300 or img.width > 300:
-			size = (300, 300)
+		if img.height > 400 or img.width > 400:
+			size = (400, 400)
 			img.thumbnail(size) # specifyin the size
 			img.save(self.image.path) #saving the resized image to original image location
 
